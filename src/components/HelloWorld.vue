2b82d0668d846e67859054a51f749cae1ef79f83
= <samuel@tedbree.com>
2020-03-14 20:12:22 +0100

    Initial Commitment

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,64 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<main class="py-4">
+  <div class="px-4">
+    <div class="block md:flex justify-between md:-mx-2">
+      <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+        <div class="bg-white rounded-lg overflow-hidden shadow relative">
+          <img class="h-56 w-full object-cover object-center" src="https://images.unsplash.com/photo-1457282367193-e3b79e38f207?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1654&q=80" alt="">
+          <div class="p-4 h-auto md:h-40 lg:h-48">
+            <a href="#" class="block text-blue-500 hover:text-blue-600 font-semibold mb-2 text-lg md:text-base lg:text-lg">
+              Woman standing under blue sky
+            </a>
+            <div class="text-gray-600 text-sm leading-relaxed block md:text-xs lg:text-sm">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quidem blanditiis unde asperiores? Officia amet perspiciatis ad quibusdam incidunt eaque, nobis, eveniet neque porro id commodi quisquam debitis!
+            </div>
+            <div class="relative mt-2 lg:absolute bottom-0 mb-4 md:hidden lg:block">
+              <a class="inline bg-gray-300 py-1 px-2 rounded-full text-xs lowercase text-gray-700" href="#">#something</a>
+              <a class="inline bg-gray-300 py-1 px-2 rounded-full text-xs lowercase text-gray-700" href="#">#sky</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+        <div class="bg-white rounded-lg overflow-hidden shadow relative">
+          <img class="h-56 w-full object-cover object-center" src="https://images.unsplash.com/photo-1465188162913-8fb5709d6d57?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="">
+          <div class="p-4 h-auto md:h-40 lg:h-48">
+            <a href="#" class="block text-blue-500 hover:text-blue-600 font-semibold mb-2 text-lg md:text-base lg:text-lg">
+              Woman walking on pathway
+            </a>
+            <div class="text-gray-600 text-sm leading-relaxed block md:text-xs lg:text-sm">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure aut quia alias ullam eveniet sunt! Ipsa, sunt. Inventore ipsum sit quasi. Alias quasi officiis blanditiis!
+            </div>
+            <div class="relative mt-2 lg:absolute bottom-0 mb-4 md:hidden lg:block">
+              <a class="inline bg-gray-300 py-1 px-2 rounded-full text-xs lowercase text-gray-700" href="#">#pathway</a>
+              <a class="inline bg-gray-300 py-1 px-2 rounded-full text-xs lowercase text-gray-700" href="#">#woman</a>
+              <a class="inline bg-gray-300 py-1 px-2 rounded-full text-xs lowercase text-gray-700" href="#">#walkalone</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+        <div class="bg-white rounded-lg overflow-hidden shadow relative">
+          <img class="h-56 w-full object-cover object-center" src="https://images.unsplash.com/photo-1467238307002-480ffdd260f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt="">
+          <div class="p-4 h-auto md:h-40 lg:h-48">
+            <a href="#" class="block text-blue-500 hover:text-blue-600 font-semibold mb-2 text-lg md:text-base lg:text-lg">
+              Walking through a forest in the afternoon
+            </a>
+            <div class="text-gray-600 text-sm leading-relaxed block md:text-xs lg:text-sm">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi nemo magni saepe cumque error quia quae sint ducimus, maiores doloremque.
+            </div>
+            <div class="relative mt-2 lg:absolute bottom-0 mb-4 md:hidden lg:block">
+              <a class="inline bg-gray-300 py-1 px-2 rounded-full text-xs lowercase text-gray-700" href="#">#forest</a>
+              <a class="inline bg-gray-300 py-1 px-2 rounded-full text-xs lowercase text-gray-700" href="#">#walk</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,10 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <button class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Button
+    </button>
   </div>
 </template>
 
